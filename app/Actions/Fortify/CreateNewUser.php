6220a8d56f441f7193e88c70af52ac4d17e48214
Jean Sacren <sakiwit@gmail.com>
2026-04-04 05:48:42 -0600

    Update CreateNewUser to auto-assign Student role

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,6 +4,9 @@ namespace App\Actions\Fortify;
 
 use App\Concerns\PasswordValidationRules;
 use App\Concerns\ProfileValidationRules;
+use App\Enums\Role;
+use App\Enums\StudentStatus;
+use App\Models\StudentProfile;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -24,10 +27,19 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => $input['password'],
         ]);
+
+        $user->assignRole(Role::Student);
+
+        $user->studentProfile()->create([
+            'student_id_number' => StudentProfile::generateIdNumber(),
+            'status' => StudentStatus::Active,
+        ]);
+
+        return $user;
     }
 }
