95a1ede9363f00f66c864856e240600d05574918
Jean Sacren <sakiwit@gmail.com>
2026-04-05 15:00:25 -0600

    Build RoomType and DayOfWeek enums

---
+++ app/Enums/DayOfWeek.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum DayOfWeek: string
+{
+    case Monday = 'monday';
+    case Tuesday = 'tuesday';
+    case Wednesday = 'wednesday';
+    case Thursday = 'thursday';
+    case Friday = 'friday';
+    case Saturday = 'saturday';
+}
---
+++ app/Enums/RoomType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum RoomType: string
+{
+    case Classroom = 'classroom';
+    case Lab = 'lab';
+    case Shop = 'shop';
+}
