3ea2dc6cad12c0930c91064f5818621a2b1e447c
Jean Sacren <sakiwit@gmail.com>
2026-04-08 13:45:20 -0600

    Update registration routes

--- routes/registration.php
+++ routes/registration.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth', 'verified', 'role:student'])->prefix('registration')->name('registration.')->group(function () {
     Route::livewire('/', 'pages::registration.sections')->name('sections');
     Route::livewire('/schedule', 'pages::registration.schedule')->name('schedule');
+    Route::livewire('/attendance', 'pages::registration.attendance')->name('attendance');
 });
