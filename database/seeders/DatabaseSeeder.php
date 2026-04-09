c4a671a7662ea357daac385c333a518207a62951
Jean Sacren <sakiwit@gmail.com>
2026-04-09 13:22:46 -0600

    Update DatabaseSeeder seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -50,6 +50,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TermSectionRoomSeeder::class,
             EnrollmentSeeder::class,
+            AttendanceSeeder::class,
         ]);
     }
 }
