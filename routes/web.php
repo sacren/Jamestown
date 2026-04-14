599e3027c6abceb13e0b3fec943031ec628f599c
Jean Sacren <sakiwit@gmail.com>
2026-04-14 14:30:32 -0600

    Add notifications route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::livewire('notifications', 'pages::notifications')->name('notifications');
 });
 
 require __DIR__.'/settings.php';
