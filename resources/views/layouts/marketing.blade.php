e97fca2f38be4367265c94e761e23dac5c55750e
Jean Sacren <sakiwit@gmail.com>
2026-04-17 23:21:37 -0600

    Add marketing layout shell

---
+++ resources/views/layouts/marketing.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    <head>
+        @include('partials.head')
+    </head>
+    <body class="min-h-screen bg-white antialiased dark:bg-neutral-900">
+        <main>
+            {{ $slot }}
+        </main>
+
+        @fluxScripts
+    </body>
+</html>
